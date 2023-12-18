5993008c2905e3f213450fcf71e59562d2bbfb98
randhavevaibhav <randhavevaibhav3@gmail.com>
2023-12-18 15:11:44 +0530

    Added source files

---
+++ Global.js
@@ -0,0 +1,2 @@
+export const API_KEY = "b4f0c70dee874788e416cac89dc56d4b";
+export const PORT = 3500;
---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import { API_KEY } from './Global.js';
+import { PORT } from './Global.js';
+const app = express();
+
+//Middleware for CORS POLICY
+app.use(cors());
+
+
+app.use(express.json());
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
+
+app.post('/getWeather', async (request,response)=>{
+   
+    try {
+        axios({
+            method: 'post',
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${request.body.city}&units=Metric&appid=${API_KEY}`
+          })
+            .then(function (Wresponse) {
+                   
+                    return response.status(201).send(Wresponse.data);
+                });
+       
+    } catch (error) {
+       console.log(`Error occured in the POST method  ${error.message}`);
+       response.send({message:error.message}); 
+    }
+
+
+})
